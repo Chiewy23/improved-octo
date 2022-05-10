bf6ab23e811125072a5df3ebd9eb75a575f2aafe
Christopher Hiew <compsci_23@pm.me>
2022-05-10 19:33:11 +0100

    Replaced with class structure

--- main.py
+++ main.py
@@ -1,150 +1,171 @@
-# Tkinter - Top create the GUI.
-# PIL (Python Image Library) - To give the GUI window an icon.
-# OS - To get the file path.
-
-# python -m pip install pillow
-
 from tkinter import *
-import tkinter.filedialog as fd
-import tkinter.messagebox as mb
-
-from PIL import Image, ImageTk
-import os
+from tkinter import messagebox
+from tkinter import filedialog
 
 
-def open_file():
-    global file
-    file = fd.askopenfilename(defaultextension=".txt", filetypes=[("All types", "*.*"), ("Text File", "*.txt*")])
-
-    if file != "":
-        root.title(f"{os.path.basename(file)}")
-        text_area.delete(1.0, END)
-        with open(file, "r") as file_:
-            text_area.insert(1.0, file_.read())
-            file_.close()
-    else:
-        file = None
+def info_about():
+    messagebox.showinfo("About Text Editor", "A Simple Text Editor\nCreated using Python.")
 
 
-def open_new_file():
-    root.title("Untitled - Didactic Tribble")
-    text_area.delete(1.0, END)
+class TextEditor:
 
+    def __init__(self, _root):
+        self.root = _root
+        self.root.title("TEXT EDITOR")
+        self.root.geometry("1200x700+200+150")
+        self.filename = None
+        self.title = StringVar()
+        self.status = StringVar()
 
-def save_file():
-    global file
-    if file == "":
-        file = None
-    else:
-        file = open(file, "w")
-        file.write(text_area.get(1.0, END))
-        file.close()
+        # ##### TITLE BAR #####
+        self.title_bar = Label(self.root, textvariable=self.title, font=("times new roman", 15, "bold"), bd=2,
+                               relief=GROOVE)
+        self.title_bar.pack(side=TOP, fill=BOTH)
+        self.set_title()
 
-    if file is None:
-        file = fd.asksaveasfilename(initialfile='Untitled.txt', defaultextension='.txt',
-                                    filetypes=[("Text File", "*.txt*"), ("Word Document", '*,docx*'), ("PDF", "*.pdf*")])
-    else:
-        file = open(file, "w")
-        file.write(text_area.get(1.0, END))
-        file.close()
-        root.title(f"{os.path.basename(file)} - Notepad")
+        # ##### STATUS BAR #####
+        self.status_bar = Label(self.root, textvariable=self.status, font=("times new roman", 15, "bold"), bd=2, relief=GROOVE)
+        self.status_bar.pack(side=BOTTOM, fill=BOTH)
+        self.status.set("Welcome To Text Editor")
 
+        # ##### MENU BAR #####
+        self.menu_bar = Menu(self.root, font=("times new roman", 15, "bold"), activebackground="skyblue")
+        # Configuring menubar on root window
+        self.root.config(menu=self.menu_bar)
 
-def exit_application():
-    root.destroy()
+        # ##### FILE MENU #####
+        self.file_menu = Menu(self.menu_bar, font=("times new roman", 12, "bold"), activebackground="skyblue", tearoff=0)
+        self.file_menu.add_command(label="New", accelerator="Ctrl+N", command=self.new_file)
+        self.file_menu.add_command(label="Open", accelerator="Ctrl+O", command=self.open_file)
+        self.file_menu.add_command(label="Save", accelerator="Ctrl+S", command=self.save_file)
+        self.file_menu.add_command(label="Save As", accelerator="Ctrl+A", command=self.save_as_file)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", accelerator="Ctrl+E", command=self.exit)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
 
+        # ##### EDIT MENU #####
+        self.edit_menu = Menu(self.menu_bar, font=("times new roman", 12, "bold"), activebackground="skyblue", tearoff=0)
+        self.edit_menu.add_command(label="Cut", accelerator="Ctrl+X", command=self.cut)
+        self.edit_menu.add_command(label="Copy", accelerator="Ctrl+C", command=self.copy)
+        self.edit_menu.add_command(label="Paste", accelerator="Ctrl+V", command=self.paste)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label="Undo", accelerator="Ctrl+U", command=self.undo)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
 
-def copy_text():
-    text_area.event_generate("<<Copy>>")
+        # ##### HELP MENU #####
+        self.help_menu = Menu(self.menu_bar, font=("times new roman", 12, "bold"), activebackground="skyblue", tearoff=0)
+        self.help_menu.add_command(label="About", command=info_about)
+        self.menu_bar.add_cascade(label="Help", menu=self.help_menu)
 
+        # ##### SCROLLBAR #####
+        scroll_y = Scrollbar(self.root, orient=VERTICAL)
+        self.text_area = Text(self.root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), state="normal", relief=GROOVE)
+        scroll_y.pack(side=RIGHT, fill=Y)
+        scroll_y.config(command=self.text_area.yview)
+        self.text_area.pack(fill=BOTH, expand=1)
 
-def cut_text():
-    text_area.event_generate("<<Cut>>")
+        self.shortcuts()
 
+    def set_title(self):
+        if self.filename:
+            self.title.set(self.filename)
+        else:
+            self.title.set("Untitled")
 
-def paste_text():
-    text_area.event_generate("<<Paste>>")
+    def new_file(self, *args):
+        self.text_area.delete("1.0", END)
+        self.filename = None
+        self.set_title()
+        self.status.set("New File Created")
 
+    def open_file(self, *args):
+        try:
+            self.filename = filedialog.askopenfilename(title="Select file", filetypes=(
+            ("All Files", "*.*"), ("Text Files", "*.txt"), ("Python Files", "*.py")))
+            if self.filename:
+                infile = open(self.filename, "r")
+                self.text_area.delete("1.0", END)
+                for line in infile:
+                    self.text_area.insert(END, line)
+                infile.close()
+                self.set_title()
+                self.status.set("Opened Successfully")
+        except Exception as e:
+            messagebox.showerror("Exception", e)
 
-def select_all():
-    text_area.event_generate("Control-Keypress-A>>")
+    def save_file(self, *args):
+        try:
+            if self.filename:
+                data = self.text_area.get("1.0", END)
+                outfile = open(self.filename, "w")
+                outfile.write(data)
+                outfile.close()
+                self.set_title()
+                self.status.set("Saved Successfully")
+            else:
+                self.save_as_file()
+        except Exception as e:
+            messagebox.showerror("Exception", e)
 
+    def save_as_file(self, *args):
+        try:
+            untitled_file = filedialog.asksaveasfilename(title="Save file As", defaultextension=".txt", initialfile="Untitled.txt", filetypes=(("All Files", "*.*"), ("Text Files", "*.txt"), ("Python Files", "*.py")))
+            data = self.text_area.get("1.0", END)
+            outfile = open(untitled_file, "w")
+            outfile.write(data)
+            outfile.close()
+            self.filename = untitled_file
+            self.set_title()
+            self.status.set("Saved Successfully")
+        except Exception as e:
+            messagebox.showerror("Exception", e)
 
-def delete_last_char():
-    text_area.event_generate("<<KP_Delete>>")
+    def exit(self, *args):
+        op = messagebox.askyesno("WARNING", "Your Unsaved Data May be Lost!!")
+        if op > 0:
+            self.root.destroy()
+        else:
+            return
 
+    def cut(self, *args):
+        self.text_area.event_generate("<<Cut>>")
 
-def about_didactic_tribble():
-    mb.showinfo("About Didactic Tribble", "One Tribble to rule them all.")
+    def copy(self, *args):
+        self.text_area.event_generate("<<Copy>>")
 
+    def paste(self, *args):
+        self.text_area.event_generate("<<Paste>>")
 
-def about_commands():
-    commands = """
-    Under the File Menu:
-    - 'New' clears the entire Text Area
-    - 'Open' clears text and opens another file
-    - 'Save As' - saves your file in the same/other extension
-    
-    Under the Edit Menu:
-    - 'Copy' copies the selected text to your clipboard
-    - 'Cut' cuts the selected text and removes it from the text area
-    - 'Paste' pastes the copied/cut text
-    - 'Select All' selects the entire text
-    - 'Delete' deletes the last character
-    """
+    def undo(self, *args):
+        try:
+            if self.filename:
+                self.text_area.delete("1.0", END)
+                infile = open(self.filename, "r")
+                for line in infile:
+                    self.text_area.insert(END, line)
+                infile.close()
+                self.set_title()
+                self.status.set("Undone Successfully")
+            else:
+                self.text_area.delete("1.0", END)
+                self.filename = None
+                self.set_title()
+                self.status.set("Undone Successfully")
+        except Exception as e:
+            messagebox.showerror("Exception", e)
 
-    mb.showinfo(title="All commands", message=commands)
+    def shortcuts(self):
+        self.text_area.bind("<Control-n>", self.new_file)
+        self.text_area.bind("<Control-o>", self.open_file)
+        self.text_area.bind("<Control-s>", self.save_file)
+        self.text_area.bind("<Control-a>", self.save_as_file)
+        self.text_area.bind("<Control-e>", self.exit)
+        self.text_area.bind("<Control-x>", self.cut)
+        self.text_area.bind("<Control-c>", self.copy)
+        self.text_area.bind("<Control-v>", self.paste)
+        self.text_area.bind("<Control-u>", self.undo)
 
 
 root = Tk()
-root.title("Untitled - Didactic Tribble")
-root.geometry("800x500")
-root.resizable(False, False)
-
-# Specify the max. number of columns and rows available in the window.
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-icon = ImageTk.PhotoImage(Image.open("pi.png"))
-root.iconphoto(False, icon)
-file = ""
-
-menu_bar = Menu(root)
-
-file_menu = Menu(menu_bar, tearoff=False, activebackground="DodgerBlue")
-edit_menu = Menu(menu_bar, tearoff=False, activebackground="DodgerBlue")
-help_menu = Menu(menu_bar, tearoff=False, activebackground="DodgerBlue")
-
-file_menu.add_command(label="New", command=open_new_file)
-file_menu.add_command(label="Open File", command=open_file)
-file_menu.add_command(label="Save As", command=save_file)
-file_menu.add_separator()
-file_menu.add_command(label="Close File", command=exit_application)
-
-edit_menu.add_command(label="Copy", command=copy_text)
-edit_menu.add_command(label="Cut", command=cut_text)
-edit_menu.add_command(label="Paste", command=paste_text)
-edit_menu.add_separator()
-edit_menu.add_command(label="Select All", command=select_all)
-edit_menu.add_command(label="Delete", command=delete_last_char)
-
-help_menu.add_command(label="About Didactic Tribble", command=about_didactic_tribble)
-help_menu.add_command(label="About Commands", command=about_commands)
-
-menu_bar.add_cascade(label="File", menu=file_menu)
-menu_bar.add_cascade(label="Edit", menu=edit_menu)
-menu_bar.add_cascade(label="Help", menu=help_menu)
-
-root.config(menu=menu_bar)
-
-text_area = Text(root, font=("Times New Roman", 12))
-text_area.grid(sticky=NSEW)
-
-scroller = Scrollbar(text_area, orient=VERTICAL)
-scroller.pack(side=RIGHT, fill=Y)
-scroller.config(command=text_area.yview)
-
-text_area.config(yscrollcommand=scroller.set)
-
-root.update()
+TextEditor(root)
 root.mainloop()
