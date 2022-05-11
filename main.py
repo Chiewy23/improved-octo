00177c0008cf311f8c5f10a899b9b9519b4a3811
Christopher Hiew <compsci_23@pm.me>
2022-05-11 19:36:14 +0100

    Minor font changes

--- main.py
+++ main.py
@@ -4,37 +4,53 @@ from tkinter import filedialog
 
 
 def info_about():
-    messagebox.showinfo("About Text Editor", "A Simple Text Editor\nCreated using Python.")
+    messagebox.showinfo("About Didactic Tribble", "Created using Python.")
 
 
 class TextEditor:
-
     def __init__(self, _root):
         self.root = _root
-        self.root.title("TEXT EDITOR")
+        self.root.title("Didactic Tribble")
         self.root.geometry("1200x700+200+150")
         self.filename = None
         self.title = StringVar()
         self.status = StringVar()
 
         # ##### TITLE BAR #####
-        self.title_bar = Label(self.root, textvariable=self.title, font=("times new roman", 15, "bold"), bd=2,
-                               relief=GROOVE)
+        self.title_bar = Label(
+            self.root,
+            textvariable=self.title,
+            font=("Helvetica", 15, "bold"),
+            bd=2,
+            relief=GROOVE
+        )
+
         self.title_bar.pack(side=TOP, fill=BOTH)
         self.set_title()
 
         # ##### STATUS BAR #####
-        self.status_bar = Label(self.root, textvariable=self.status, font=("times new roman", 15, "bold"), bd=2, relief=GROOVE)
+        self.status_bar = Label(
+            self.root,
+            textvariable=self.status,
+            font=("Helvetica", 15, "bold"),
+            bd=2,
+            relief=GROOVE)
+
         self.status_bar.pack(side=BOTTOM, fill=BOTH)
         self.status.set("Welcome To Text Editor")
 
         # ##### MENU BAR #####
-        self.menu_bar = Menu(self.root, font=("times new roman", 15, "bold"), activebackground="skyblue")
-        # Configuring menubar on root window
+        self.menu_bar = Menu(self.root, font=("Helvetica", 15, "bold"), activebackground="white")
         self.root.config(menu=self.menu_bar)
 
         # ##### FILE MENU #####
-        self.file_menu = Menu(self.menu_bar, font=("times new roman", 12, "bold"), activebackground="skyblue", tearoff=0)
+        self.file_menu = Menu(
+            self.menu_bar,
+            font=("Helvetica", 12, "bold"),
+            activebackground="white",
+            tearoff=0
+        )
+
         self.file_menu.add_command(label="New", accelerator="Ctrl+N", command=self.new_file)
         self.file_menu.add_command(label="Open", accelerator="Ctrl+O", command=self.open_file)
         self.file_menu.add_command(label="Save", accelerator="Ctrl+S", command=self.save_file)
@@ -44,7 +60,13 @@ class TextEditor:
         self.menu_bar.add_cascade(label="File", menu=self.file_menu)
 
         # ##### EDIT MENU #####
-        self.edit_menu = Menu(self.menu_bar, font=("times new roman", 12, "bold"), activebackground="skyblue", tearoff=0)
+        self.edit_menu = Menu(
+            self.menu_bar,
+            font=("Helvetica", 12, "bold"),
+            activebackground="white",
+            tearoff=0
+        )
+
         self.edit_menu.add_command(label="Cut", accelerator="Ctrl+X", command=self.cut)
         self.edit_menu.add_command(label="Copy", accelerator="Ctrl+C", command=self.copy)
         self.edit_menu.add_command(label="Paste", accelerator="Ctrl+V", command=self.paste)
@@ -53,17 +75,29 @@ class TextEditor:
         self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
 
         # ##### HELP MENU #####
-        self.help_menu = Menu(self.menu_bar, font=("times new roman", 12, "bold"), activebackground="skyblue", tearoff=0)
+        self.help_menu = Menu(
+            self.menu_bar,
+            font=("Helvetica", 12, "bold"),
+            activebackground="white",
+            tearoff=0
+        )
+
         self.help_menu.add_command(label="About", command=info_about)
         self.menu_bar.add_cascade(label="Help", menu=self.help_menu)
 
         # ##### SCROLLBAR #####
         scroll_y = Scrollbar(self.root, orient=VERTICAL)
-        self.text_area = Text(self.root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), state="normal", relief=GROOVE)
+        self.text_area = Text(
+            self.root,
+            yscrollcommand=scroll_y.set,
+            font=("times new roman", 15, "bold"),
+            state="normal",
+            relief=GROOVE
+        )
+
         scroll_y.pack(side=RIGHT, fill=Y)
         scroll_y.config(command=self.text_area.yview)
         self.text_area.pack(fill=BOTH, expand=1)
-
         self.shortcuts()
 
     def set_title(self):
@@ -80,8 +114,11 @@ class TextEditor:
 
     def open_file(self, *args):
         try:
-            self.filename = filedialog.askopenfilename(title="Select file", filetypes=(
-            ("All Files", "*.*"), ("Text Files", "*.txt"), ("Python Files", "*.py")))
+            self.filename = filedialog.askopenfilename(
+                title="Select file",
+                filetypes=(("All Files", "*.*"), ("Text Files", "*.txt"), ("Python Files", "*.py"))
+            )
+
             if self.filename:
                 infile = open(self.filename, "r")
                 self.text_area.delete("1.0", END)
@@ -109,7 +146,13 @@ class TextEditor:
 
     def save_as_file(self, *args):
         try:
-            untitled_file = filedialog.asksaveasfilename(title="Save file As", defaultextension=".txt", initialfile="Untitled.txt", filetypes=(("All Files", "*.*"), ("Text Files", "*.txt"), ("Python Files", "*.py")))
+            untitled_file = filedialog.asksaveasfilename(
+                title="Save file As",
+                defaultextension=".txt",
+                initialfile="Untitled.txt",
+                filetypes=(("All Files", "*.*"), ("Text Files", "*.txt"), ("Python Files", "*.py"))
+            )
+
             data = self.text_area.get("1.0", END)
             outfile = open(untitled_file, "w")
             outfile.write(data)
