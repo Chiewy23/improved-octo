f21bdaea5eb96853edebbf7f4538aa309c41ba3d
Christopher Hiew <christopherhiew@protonmail.com>
2022-06-19 15:40:54 +0100

    Initial start to adding accented character sets.

---
+++ character_sets/greek_accented_uppercase.py
@@ -0,0 +1,18 @@
+CHARACTER_SET = [
+    "Ά",
+    "Έ",
+    "Ή",
+    "Ί",
+    "Ό",
+    "Ύ",
+    "Ώ",
+    "Ὰ",
+    "Ὲ",
+    "Ὴ",
+    "Ὶ",
+    "Ὸ",
+    "Ὺ",
+    "Ὼ"
+]
+
+# https://en.wikipedia.org/wiki/Greek_diacritics
